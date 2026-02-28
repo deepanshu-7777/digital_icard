2855d5755ef890b548cf3bdd754bcc25e15bb289
Deepanshu Tayal <deepanshutayal777@gmail.com>
2026-02-28 15:37:36 +0530

    card design

--- download.php
+++ download.php
@@ -10,20 +10,17 @@ $result=mysqli_query($conn,
 "SELECT * FROM employees WHERE employeeid='$id'");
 
 $row=mysqli_fetch_assoc($result);
-
 $pdf=new FPDF();
 
 $pdf->AddPage();
 
-/* Company Logo */
+/* Logo */
 
 $pdf->Image("photos/omaxe_logo.jpeg",70,10,60);
 
-/* Move Cursor Down */
-
 $pdf->Ln(35);
 
-/* Company Name */
+/* Title */
 
 $pdf->SetFont("Arial","B",16);
 
@@ -31,17 +28,23 @@ $pdf->Cell(190,10,"OMAXE Employee ID Card",0,1,"C");
 
 $pdf->Ln(10);
 
+/* Employee Details Centered */
+
 $pdf->SetFont("Arial","",12);
 
-$pdf->Cell(50,10,"Employee ID:");
-$pdf->Cell(100,10,$row['employeeid'],0,1);
+$pdf->Cell(190,10,"Employee ID: ".$row['employeeid'],0,1,"C");
 
- $pdf->Cell(50,10,"Name:");
-$pdf->Cell(100,10,$row['name'],0,1);
+$pdf->Cell(190,10,"Name: ".$row['name'],0,1,"C");
 
-$pdf->Cell(50,10,"Department:");
-$pdf->Cell(100,10,$row['department'],0,1);
+$pdf->Cell(190,10,"Department: ".$row['department'],0,1,"C");
 
+$pdf->Ln(10);
+
+/* QR Code Center */
+
+$pdf->Image($row['qr'],80,120,50);
+
+$pdf->Output("D",$row['employeeid']."_card.pdf");
 // $pdf->Cell(50,10,"Phone:");
 // $pdf->Cell(100,10,$row['phone'],0,1);
 
@@ -56,11 +59,11 @@ $pdf->Cell(100,10,$row['department'],0,1);
 
 /* QR Code */
 
-$pdf->Image($row['qr'],150,80,40);
+// $pdf->Image($row['qr'],150,80,40);
 
-// $pdf->Cell(50,10,"company address:");
-// $pdf->Cell(100,10,$row['company_address'],0,1);
+// // $pdf->Cell(50,10,"company address:");
+// // $pdf->Cell(100,10,$row['company_address'],0,1);
 
-$pdf->Output("D",$row['employeeid']."_card.pdf");
+// $pdf->Output("D",$row['employeeid']."_card.pdf");
 
-?>
+// ?>
